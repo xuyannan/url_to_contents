515c58d6174c670257a735e43cde490903aec930
skywalker <skywalker@skywalker-VirtualBox.(none)>
2012-10-18 02:34:32 +0800

    fix file format

--- lib/url_to_contents/parser.rb
+++ lib/url_to_contents/parser.rb
@@ -2,16 +2,16 @@
 
 module UrlToContents
 
-	class Parser
-		def initialize(doc)
-				@doc = doc
-		end
-
-		def youku
-			@doc.css('#panel_share > .panel_con > .p1 > .item').each do |u|
-				return u.xpath('input').attr('value').value  if u.css('.label').children.text  == "flash地址: "
-			end		
-		end
-	end
+  class Parser
+    def initialize(doc)
+      @doc = doc
+    end
+    
+    def youku
+      @doc.css('#panel_share > .panel_con > .p1 > .item').each do |u|
+        return u.xpath('input').attr('value').value  if u.css('.label').children.text  == "flash地址: "
+      end		
+    end
+  end
 	
 end
